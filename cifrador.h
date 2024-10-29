2c301c371054aef923232a1e798a1822b47fe6d8
Ubuntu <ubuntu@ip-172-31-11-233.us-east-2.compute.internal>
2024-10-29 15:50:16 +0000

    lib practice

---
+++ cifrador.h
@@ -0,0 +1,12 @@
+// cifrador.h
+#ifndef CIFRADOR_H
+#define CIFRADOR_H
+
+void cifrarCesar(char mensaje[], int desplazamiento);
+void descifrarCesar(char mensaje[], int desplazamiento);
+void cifrarEscitala(char mensaje[], int numLineas);
+void descifrarEscitala(char mensaje[], int numLineas);
+void mostrarInfoProgramadores();
+
+#endif
+
